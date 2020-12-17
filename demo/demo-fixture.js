087a390096e0f6743c88c306722ff5503345f532
Trent Brown <trent.brown@mx.com>
2020-12-16 19:59:26 -0800

    WIP

--- demo/demo-fixture.js
+++ demo/demo-fixture.js
@@ -26,7 +26,6 @@ A behavior element that lets us copy text to the browser clipboard.
   from HTML and may be out of place here. Review them and
   then delete this comment!
 */
-import 'ft-element-demo/ft-element-demo.js';
 
 import '@polymer/iron-flex-layout/iron-flex-layout-classes.js';
 import '@polymer/polymer/polymer-legacy.js';
